e742f9ad559e9f4bc6e9bc8bc3c893efe428672d
Maitry <41632558+MaitryG@users.noreply.github.com>
2021-11-22 12:44:32 +0530

    Add files via upload

---
+++ lib/quiz_brain.dart
@@ -0,0 +1,14 @@
+import 'package:quizzler/question.dart';
+
+class QuizBrain{
+  List<Question> quesAns = [
+    Question(q:'You can lead a cow downstairs but not upstairs',a:false),
+    Question(q:'Despite their brain size, elephant memory is short-lived.',a:false),
+    Question(q:'Penguins can\'t be found on the continent of Africa.',a:false),
+    Question(q:'Horses sleep standing up', a:true),
+    Question(q:'You can\'t teach an old dog new tricks',a:false),
+    Question(q:'Bats are not blind',a:true),
+    Question(q:'Crocodiles shed tears while eating',a:true),
+    Question(q:'Axolotl(relative of spotted salamanders) can regenerate missing limbs and organs',a:true),
+  ];
+}
